858c47a8b397dca318bcf06ae2a20c8221e234e9
Lisa Heffernan <lisaheffernan@outlook.com>
2023-01-06 20:09:08 +0000

    Message test passed - 'Enjoy your game' displayed when game is started

--- src/components/Message.js
+++ src/components/Message.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 const Message = () => {
-  return <div className="message">message</div>;
+  return <div className="message">Enjoy your game</div>;
 };
 
 export default Message;
--- src/tests/Message.test.js
+++ src/tests/Message.test.js
@@ -1,9 +1,15 @@
-import React, { render } from "@testing-library/react";
+import React, { render, screen } from "@testing-library/react";
 import Message from "../components/Message";
 
 describe("Message", () => {
   it("renders correctly", () => {
     const { asFragment } = render(<Message />);
     expect(asFragment()).toMatchSnapshot();
+  });
+
+  it("renders correct message upon game started", () => {
+    render(<Message />);
+
+    expect(screen.getByText("Enjoy your game")).toHaveClass("message");
   });
 });
