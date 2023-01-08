f51757c4fbefc358bd1c3c61ec28a0018677404a
Lisa Heffernan <lisaheffernan@outlook.com>
2023-01-08 10:08:16 +0000

    Integrated object tests into files and separated them fromm the component test

---
+++ src/tests/objects-tests/Deck.unit.test.js
@@ -0,0 +1,72 @@
+const Deck = require("../../objects/Deck");
+
+describe("Deck", () => {
+  describe("Deck instantiation", () => {
+    let deck;
+
+    beforeEach(() => {
+      deck = new Deck();
+    });
+
+    it("Can be instantiated", () => {
+      expect(deck).toBeInstanceOf(Object);
+    });
+
+    it("initiateDeck is a method", () => {
+      expect(typeof deck.initiateDeck).toBe("function");
+    });
+
+    it("populateSuit is a method", () => {
+      expect(typeof deck.populateSuit).toBe("function");
+    });
+  });
+
+  describe("Deck has been populated correctly", () => {
+    let deck;
+    let unshuffledDeck;
+
+    beforeEach(() => {
+      deck = new Deck();
+      deck.initiateDeck();
+      unshuffledDeck = deck.cards;
+    });
+
+    it("Deck has 52 cards upon deck being populated", () => {
+      expect(unshuffledDeck.length).toEqual(52);
+    });
+
+    it("There are 13 of each suit", () => {
+      const suits = ["♣️", "♥️", "♠️", "♦️"];
+      const sortedSuits = suits.map((suit) =>
+        unshuffledDeck.filter((card) => card.cardSuit === suit)
+      );
+
+      sortedSuits.forEach((sortedSuit) =>
+        expect(sortedSuit.length).toEqual(13)
+      );
+    });
+
+    it("There are 4 of each rank", () => {
+      const ranks = [
+        "A",
+        "2",
+        "3",
+        "4",
+        "5",
+        "6",
+        "7",
+        "8",
+        "9",
+        "10",
+        "J",
+        "Q",
+        "K",
+      ];
+      const sortedRanks = ranks.map((rank) =>
+        unshuffledDeck.filter((card) => card.cardRank === rank)
+      );
+
+      sortedRanks.forEach((sortedRank) => expect(sortedRank.length).toEqual(4));
+    });
+  });
+});
