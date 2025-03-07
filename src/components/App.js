385ac87e46e3ce2f4343c0f587c5ea48b490a0a8
LCHeffernan <lisa.heffernan@bbc.co.uk>
2025-03-07 11:03:19 +0000

    displays player 2 hand with basic styling

--- src/components/App.js
+++ src/components/App.js
@@ -10,7 +10,12 @@ import Hand from "../objects/Hand";
 import "../styles/app.css";
 
 const App = () => {
-  const [playerHand, setPlayerHand] = useState({
+  // const numberOfPlayers = 2;
+  const [playerHand1, setPlayerHand1] = useState({
+    isHandValid: true,
+    isGameOver: true,
+  });
+  const [playerHand2, setPlayerHand2] = useState({
     isHandValid: true,
     isGameOver: true,
   });
@@ -18,6 +23,7 @@ const App = () => {
     isHandValid: true,
   });
   const [handObject, setHandObject] = useState({});
+  const [handObject2, setHandObject2] = useState({});
   const [dealerHandObject, setDealerHandObject] = useState({});
 
   const handleNewGame = () => {
@@ -25,20 +31,31 @@ const App = () => {
     deck.initiateDeck();
     const dealer = new Dealer(deck);
     const hand = new Hand(dealer);
+    const hand2 = new Hand(dealer);
     const handDealer = new Hand(dealer);
 
     hand.hitMe();
+    hand2.hitMe();
     handDealer.hitMe();
     hand.hitMe();
+    hand2.hitMe();
     handDealer.hitMe();
 
     setHandObject(hand);
+    setHandObject2(hand2);
     setDealerHandObject(handDealer);
-    setPlayerHand({
+
+    setPlayerHand1({
       playerHand: hand.playerHand,
       playerScore: hand.playerScore,
       isHandValid: hand.isHandValid,
       isGameOver: hand.isGameOver,
+    });
+    setPlayerHand2({
+      playerHand: hand2.playerHand,
+      playerScore: hand2.playerScore,
+      isHandValid: hand2.isHandValid,
+      isGameOver: hand2.isGameOver,
     });
     setDealerHand({
       dealerHand: [handDealer.playerHand[0], {}],
@@ -50,7 +67,7 @@ const App = () => {
 
   const handleHitMe = () => {
     handObject.hitMe();
-    setPlayerHand({
+    setPlayerHand1({
       playerHand: handObject.playerHand,
       playerScore: handObject.playerScore,
       isHandValid: handObject.isHandValid,
@@ -60,22 +77,22 @@ const App = () => {
 
   const handleStand = () => {
     handObject.stand();
-    setPlayerHand({
-      ...playerHand,
+    setPlayerHand1({
+      ...playerHand1,
       isGameOver: handObject.isGameOver,
     });
   };
 
   useEffect(() => {
-    if (playerHand.isGameOver) {
+    if (playerHand1.isGameOver) {
       setDealerHand({
         ...dealerHand,
         dealerHand: dealerHandObject.playerHand,
       });
     }
     if (
-      playerHand.isGameOver &&
-      playerHand.playerScore < 21 &&
+      playerHand1.isGameOver &&
+      playerHand1.playerScore < 21 &&
       dealerHand.dealerScore < 17
     ) {
       dealerHandObject.hitMe();
@@ -88,8 +105,8 @@ const App = () => {
     }
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [
-    playerHand.isHandValid,
-    playerHand.isGameOver,
+    playerHand1.isHandValid,
+    playerHand1.isGameOver,
     dealerHand.dealerScore,
     dealerHandObject,
   ]);
@@ -102,37 +119,46 @@ const App = () => {
 ♠️ ♥️ BLACKJACK TABLE ♣️ ♦️
 ♥️ ♣️ ♦️ ♠️ ♥️ ♣️ ♦️ ♠️ ♥️ ♣️ ♦️ ♠️`}
         </div>
-        {playerHand.playerHand && (
+        {playerHand1.playerHand && (
           <DealerPlayer
             className="dealer"
             dealerHand={dealerHand.dealerHand}
             dealerScore={dealerHand.dealerScore}
-            isPlayerGameOver={playerHand.isGameOver}
+            isPlayerGameOver={playerHand1.isGameOver}
           />
         )}
-        {playerHand.playerHand && (
-          <Player
-            className="player"
-            playerHand={playerHand.playerHand}
-            playerScore={playerHand.playerScore}
-          />
-        )}
+        <div className="playerWrapper">
+          {playerHand1.playerHand && (
+            <Player
+              className="player1"
+              playerHand={playerHand1.playerHand}
+              playerScore={playerHand1.playerScore}
+            />
+          )}
+          {playerHand2.playerHand && (
+            <Player
+              className="player2"
+              playerHand={playerHand2.playerHand}
+              playerScore={playerHand2.playerScore}
+            />
+          )}
+        </div>
         <div className="message">
           <Message
             className="message-text"
-            playerHand={playerHand}
+            playerHand={playerHand1}
             dealerHand={dealerHand}
           />
         </div>
         <div className="buttons">
           <NewGame
             handleNewGame={handleNewGame}
-            isGameOver={playerHand.isGameOver}
+            isGameOver={playerHand1.isGameOver}
           />
           <PlayButtons
             handleHitMe={handleHitMe}
             handleStand={handleStand}
-            isGameOver={playerHand.isGameOver}
+            isGameOver={playerHand1.isGameOver}
           />
         </div>
       </div>
